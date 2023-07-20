cb155a688d4762a6e471b6d0b7c5e8c06a7f8dff
principal <rafoelhonrado@hotmail.com>
2023-07-19 22:37:50 -0500

    FIX-10-02, Fix for security configuration in spring discovery server.

--- spring-discovery-server/src/main/java/com/vichamalab/discovery/config/SecurityConfig.java
+++ spring-discovery-server/src/main/java/com/vichamalab/discovery/config/SecurityConfig.java
@@ -2,15 +2,14 @@ package com.vichamalab.discovery.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-//@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -22,29 +21,11 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(user);
     }
     
-	/*@Override
-	protected void configure(HttpSecurity http) throws Exception
-	{
-		http
-			.authorizeRequests()
-			.anyRequest().authenticated()
-			.and()
-			.httpBasic()
-			.and()
-			.csrf().disable();
-	}*/
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-        	.authorizeRequests()
-        	.anyRequest()
-        	.authenticated()
-        	.and()
-        	.httpBasic();
-          /*  .authorizeHttpRequests((authz) -> authz
-                .anyRequest().authenticated()
-            )
-            .httpBasic();*/
+        http.csrf(csrf -> csrf.disable())
+        	.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+        	 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
